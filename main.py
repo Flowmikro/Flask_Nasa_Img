8a0590863ee4c92df1e9bb404bfdf79bdbd99cfe
Flowmikro <cloplop091@gmail.com>
2023-08-08 16:46:10 +0300

    img_nasa

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+import requests
+import json
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def mars_photos():
+    nasa_api = requests.get('https://api.nasa.gov/mars-photos/api/v1/rovers/Perseverance/latest_photos?api_key=DEMO_KEY')
+    jasondata = json.loads(nasa_api.text)
+    phohtos = jasondata['latest_photos']
+    return render_template('mars_photos.html', photos=phohtos)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
